03bf9ccbf578e37fd21b88ed29284533213a083d
Sergioramos228 <serega170503@gmail.com>
2023-12-05 23:06:26 +0100

    Replace files

--- Assets/Scripts/Weapon/Gun.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Gun : Weapon
-{
-    public override void Shoot(Transform shootPoint)
-    {
-        Instantiate(Bullet, shootPoint.position, Quaternion.identity);
-    }
-}
